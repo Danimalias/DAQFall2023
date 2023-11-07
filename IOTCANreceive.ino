2b23fa31a03dae7615302b114ccc16c0b9926efd
Dionna Parina <68387555+DionnaParina@users.noreply.github.com>
2023-11-06 23:42:21 -0500

    EOD 11/6/23
    trying to send back joystick data

--- IOTCANreceive.ino
+++ IOTCANreceive.ino
@@ -2,8 +2,8 @@
 #include <CAN_config.h>
 /* the variable name CAN_cfg is fixed, do not change */
 
-#define VRX_PIN  26 // ESP32 pin GPIO36 (ADC0) connected to VRX pin
 //A0 is 26 A1 is 25
+#define VRX_PIN  26 // ESP32 pin GPIO36 (ADC0) connected to VRX pin
 #define VRY_PIN  25 // ESP32 pin GPIO39 (ADC0) connected to VRY pin
 
 int valueX = 0; // to store the X-axis value
@@ -24,35 +24,63 @@ void setup() {
 }
 void loop() {
   sendBack();
+  //sendJoystick1();
 }
 
 void sendBack() {
   CAN_frame_t rx_frame;
   //receive next CAN frame from queue
-  if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
-    if (rx_frame.FIR.B.FF == CAN_frame_std) {
+  // if it received something, send it back
+    if(xQueueReceive(CAN_cfg.rx_queue,&rx_frame, 3*portTICK_PERIOD_MS)==pdTRUE){
+    if(rx_frame.FIR.B.FF==CAN_frame_std)
       printf("New standard frame");
-    }
-    else {
+    else
       printf("New extended frame");
+    if(rx_frame.FIR.B.RTR==CAN_RTR)
+      printf(" RTR from 0x%08x, DLC %d\r\n",rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+    else{
+      printf(" from 0x%08x, DLC %d\n",rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+      for(int i = 0; i < 8; i++){
+        printf("%c\t", (char)rx_frame.data.u8[i]);
+      }
+      printf("\n");
     }
-    if (rx_frame.FIR.B.RTR == CAN_RTR) {
-      printf(" RTR from 0x%08x, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-    }
-    else {
-      printf(" from 0x%08x, DLC %d\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-    }
-    for (int i = 0; i < rx_frame.FIR.B.DLC; i++) {
-      Serial.println(rx_frame.data.u8[i]);
-      //printf(rx_frame.data.u8[i]);
-      //ESP32Can.CANWriteFrame(&rx_frame);
-    }
-    sendJoystick(); 
+  }
+  else{
+    Serial.println("--reached else going to send joystick");
+    //sendJoystick();
+    // read X and Y analog values
+    unsigned int valueX = analogRead(VRX_PIN);
+    unsigned int valueY = analogRead(VRY_PIN);
+    valueX = byte(valueX); 
+    valueY = byte(valueY); 
+    
+    uint8_t digitx0 = valueX / 100;
+    uint8_t digitx1 = (valueX / 10) % 10;
+    uint8_t digitx2 = valueX % 10;
+  
+    uint8_t digity0 = valueY / 100;
+    uint8_t digity1 = (valueY / 10) % 10;
+    uint8_t digity2 = valueY % 10;
+    
+    Serial.print("x = ");
+    Serial.println(valueX);
+    Serial.print(", y = ");
+    Serial.println(valueY);
+    rx_frame.FIR.B.FF = CAN_frame_std;
+    rx_frame.MsgID = 2;
+    rx_frame.FIR.B.DLC = 8;
+    rx_frame.data.u8[0] = char(digitx0);
+    rx_frame.data.u8[1] = char(digitx1);
+    rx_frame.data.u8[2] = char(digitx2);
+    rx_frame.data.u8[4] = char(digity0);
+    rx_frame.data.u8[5] = char(digity1);
+    rx_frame.data.u8[6] = char(digity2);
     ESP32Can.CANWriteFrame(&rx_frame);
   }
 }
-
-void sendJoystick () {
+ 
+void sendJoystick() {
   CAN_frame_t rx_frame;
   // read X and Y analog values
   unsigned int valueX = analogRead(VRX_PIN);
@@ -60,32 +88,26 @@ void sendJoystick () {
   valueX = byte(valueX); 
   valueY = byte(valueY); 
   
-  uint8_t digitx0 = valueX / 1000;
-  uint8_t digitx1 = valueX / 100;
-  uint8_t digitx2 = (valueX / 10) % 10;
-  uint8_t digitx3 = valueX % 10;
+  uint8_t digitx0 = valueX / 100;
+  uint8_t digitx1 = (valueX / 10) % 10;
+  uint8_t digitx2 = valueX % 10;
 
-  uint8_t digity0 = valueY / 1000;
-  uint8_t digity1 = valueY / 100;
-  uint8_t digity2 = (valueY / 10) % 10;
-  uint8_t digity3 = valueY % 10;
-
-  // print data to Serial Monitor on Arduino IDE
+  uint8_t digity0 = valueY / 100;
+  uint8_t digity1 = (valueY / 10) % 10;
+  uint8_t digity2 = valueY % 10;
+  
   Serial.print("x = ");
   Serial.println(valueX);
   Serial.print(", y = ");
   Serial.println(valueY);
-
   rx_frame.FIR.B.FF = CAN_frame_std;
   rx_frame.MsgID = 2;
   rx_frame.FIR.B.DLC = 8;
   rx_frame.data.u8[0] = char(digitx0);
   rx_frame.data.u8[1] = char(digitx1);
   rx_frame.data.u8[2] = char(digitx2);
-  rx_frame.data.u8[3] = char(digitx3);
   rx_frame.data.u8[4] = char(digity0);
   rx_frame.data.u8[5] = char(digity1);
   rx_frame.data.u8[6] = char(digity2);
-  rx_frame.data.u8[7] = char(digity3);
   ESP32Can.CANWriteFrame(&rx_frame);
 }
--- IOTCANsendTemp.ino
+++ IOTCANsendTemp.ino
@@ -8,26 +8,29 @@ int DHpin = A8;
 
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println("iotsharing.com CAN demo");
-    /* set CAN pins and baudrate */
-    CAN_cfg.speed=CAN_SPEED_1000KBPS;
-    CAN_cfg.tx_pin_id = GPIO_NUM_5;
-    CAN_cfg.rx_pin_id = GPIO_NUM_4;
-    /* create a queue for CAN receiving */
-    CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
-    //initialize CAN Module
-    ESP32Can.CANInit();
+  Serial.begin(115200);
+  Serial.println("iotsharing.com CAN demo");
+  /* set CAN pins and baudrate */
+  CAN_cfg.speed=CAN_SPEED_1000KBPS;
+  CAN_cfg.tx_pin_id = GPIO_NUM_5;
+  CAN_cfg.rx_pin_id = GPIO_NUM_4;
+  /* create a queue for CAN receiving */
+  CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
+  //initialize CAN Module
+  ESP32Can.CANInit();
 }
 
 void loop() {
-    CAN_frame_t rx_frame;
-    unsigned int temp = analogRead(DHpin);
-    temp = (byte)temp;
-    uint8_t digit1 = temp / 100;
-    uint8_t digit2 = (temp / 10) % 10;
-    uint8_t digit3 = temp % 10;
-    //receive next CAN frame from queue
+  // always collecting Temp data 
+  collectTemp();
+  //receive next CAN frame from queue
+  CAN_frame_t rx_frame;
+  rx_frame.FIR.B.FF = CAN_frame_std;
+  rx_frame.MsgID = 2;
+  rx_frame.FIR.B.DLC = 8;
+//  if(xQueueReceive(CAN_cfg.rx_queue,&rx_frame, 3*portTICK_PERIOD_MS)==pdTRUE){
+//    receiveData();
+//  }
     if(xQueueReceive(CAN_cfg.rx_queue,&rx_frame, 3*portTICK_PERIOD_MS)==pdTRUE){
       if(rx_frame.FIR.B.FF==CAN_frame_std){
         //printf("New standard frame");
@@ -55,63 +58,70 @@ void loop() {
       int z = rx_frame.data.u8[2];
       printf("%d%d%d",x,y,z);
       //make function to concat all the separate ints into one in and put it here 
-      //Serial.println("data Recieved");
+      //Serial.println("data Recieved"); 
       //printf("%c\t",dataReceived);
     }
-    //next: put this data together into one integer 
-    else {
-      //if not receiving, we send data 
-      //do this simultaneously? 
-      //rx_frame.FIR.B.FF = CAN_frame_std;
-      //sendTemp();
-
-      CAN_frame_t rx_frame;
-      rx_frame.FIR.B.FF = CAN_frame_std; 
-      rx_frame.MsgID = 1;
-      rx_frame.FIR.B.DLC = 8;
-      rx_frame.data.u8[0] = char(digit1); 
-      rx_frame.data.u8[1] = char(digit2); 
-      rx_frame.data.u8[2] = char(digit3); 
-      Serial.println("-------temp-------");
-      Serial.println(temp);
-      Serial.println("-------digit1-------");
-      Serial.println(digit1);
-      Serial.println("-------digit2-------");
-      Serial.println(digit2);
-      Serial.println("-------digit3-------");
-      Serial.println(digit3);
-      ESP32Can.CANWriteFrame(&rx_frame);
-    }
+   else {
+    //sendTemp();
+  }
 }
 
-//void sendTemp(){
-//    CAN_frame_t rx_frame;
-//    rx_frame.FIR.B.FF = CAN_frame_std;
-//    rx_frame.MsgID = 1;
-//    rx_frame.FIR.B.DLC = 8;
-//    rx_frame.data.u8[0] = char(digit1); 
-//    rx_frame.data.u8[1] = char(digit2); 
-//    rx_frame.data.u8[2] = char(digit3); 
-//    Serial.println("-------temp-------");
-//    Serial.println(temp);
-//    Serial.println("-------digit1-------");
-//    Serial.println(digit1);
-//    Serial.println("-------digit2-------");
-//    Serial.println(digit2);
-//    Serial.println("-------digit3-------");
-//    Serial.println(digit3);
-//    ESP32Can.CANWriteFrame(&rx_frame);
-//    //delay(200);
-//}
+void sendTemp(){
+  unsigned int temp = analogRead(DHpin);
+  temp = (byte)temp;
+  uint8_t digit1 = temp / 100;
+  uint8_t digit2 = (temp / 10) % 10;
+  uint8_t digit3 = temp % 10;
+  CAN_frame_t rx_frame;
+  rx_frame.FIR.B.FF = CAN_frame_std;
+  rx_frame.MsgID = 1;
+  rx_frame.FIR.B.DLC = 8;
+  rx_frame.data.u8[0] = char(digit1); 
+  rx_frame.data.u8[1] = char(digit2); 
+  rx_frame.data.u8[2] = char(digit3); 
+  Serial.println("-------temp-------");
+  Serial.println(temp);
+  Serial.println("-------digit1-------");
+  Serial.println(digit1);
+  Serial.println("-------digit2-------");
+  Serial.println(digit2);
+  Serial.println("-------digit3-------");
+  Serial.println(digit3);
+  ESP32Can.CANWriteFrame(&rx_frame);
+  //delay(200);
+}
+void collectTemp(){
+  unsigned int temp = analogRead(DHpin);
+  Serial.println("-------temp-------");
+  Serial.println(temp);
+  Serial.println("-------end temp-------");
+}
 
-//int conversion(uint_8 data) {
-//  // convert unsigned int to just an integer 
-//  // modulo the number into separate digits 
-//  // put the digits into an integer array 
-//}
-//      rx_frame.data.u8[2] = 'l';
-//      rx_frame.data.u8[3] = 'l';
-//      rx_frame.data.u8[4] = 'o';
-//      rx_frame.data.u8[5] = 'c';
-//      rx_frame.data.u8[6] = 'a';
-//      rx_frame.data.u8[7] = 'n';
+void receiveData(){
+  CAN_frame_t rx_frame;
+  if(rx_frame.FIR.B.FF==CAN_frame_std){
+    //printf("New standard frame");
+    printf("received from other node");
+  }
+  else {
+    //printf("New extended frame");
+    printf("received from other node");
+  }
+  if(rx_frame.FIR.B.RTR==CAN_RTR){
+    printf(" RTR from 0x%08x, DLC %d\r\n",rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+  }
+  else{
+    printf(" from 0x%08x, DLC %d\n",rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+  }
+  // print received data to terminal 
+  int x = rx_frame.data.u8[0];
+  int y = rx_frame.data.u8[1];
+  int z = rx_frame.data.u8[2];
+  // concat digits into one integer
+  Serial.println("Received: ");
+  //Serial.println(x,y,z);
+  Serial.println(x);
+  Serial.println(y);
+  Serial.println(z);
+  //printf("%d%d%d",x,y,z);
+}
