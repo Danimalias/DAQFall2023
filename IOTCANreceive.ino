233f7cdc2c1e49a41dfad97976d2c995b9c35539
Danimalias <91852453+Danimalias@users.noreply.github.com>
2023-11-18 10:30:12 -0500

    added RPM sensor integration

--- IOTCANreceive.ino
+++ IOTCANreceive.ino
@@ -9,22 +9,34 @@
 int valueX = 0; // to store the X-axis value
 int valueY = 0; // to store the Y-axis value
 
+// rpm stuff
+// digital pin 2 is the hall pin
+int hall_pin = 13;
+// set number of hall trips for RPM reading (higher improves accuracy)
+float total_time = 0;
+int hall_count = 0;
+
+
 CAN_device_t CAN_cfg;
 void setup() {
   Serial.begin(115200);
   Serial.println("iotsharing.com CAN demo");
+  // make the hall pin an input:
+  pinMode(hall_pin, INPUT);
   /* set CAN pins and baudrate */
   CAN_cfg.speed = CAN_SPEED_1000KBPS;
   CAN_cfg.tx_pin_id = GPIO_NUM_5;
   CAN_cfg.rx_pin_id = GPIO_NUM_4;
   /* create a queue for CAN receiving */
   CAN_cfg.rx_queue = xQueueCreate(10, sizeof(CAN_frame_t));
+
   //initialize CAN Module
   ESP32Can.CANInit();
 }
 void loop() {
   sendBack();
-  //sendJoystick1();
+  //sendJoystick();
+  rpm(); 
 }
 
 void sendBack() {
@@ -73,9 +85,13 @@ void sendBack() {
     rx_frame.data.u8[0] = char(digitx0);
     rx_frame.data.u8[1] = char(digitx1);
     rx_frame.data.u8[2] = char(digitx2);
-    rx_frame.data.u8[4] = char(digity0);
-    rx_frame.data.u8[5] = char(digity1);
-    rx_frame.data.u8[6] = char(digity2);
+    rx_frame.data.u8[3] = char(digity0);
+    rx_frame.data.u8[4] = char(digity1);
+    rx_frame.data.u8[5] = char(digity2);
+
+    Serial.println("Sending RPM Data: 0/1 and Time Elapsed"); 
+    rpm(); 
+    
     ESP32Can.CANWriteFrame(&rx_frame);
   }
 }
@@ -110,4 +126,42 @@ void sendJoystick() {
   rx_frame.data.u8[5] = char(digity1);
   rx_frame.data.u8[6] = char(digity2);
   ESP32Can.CANWriteFrame(&rx_frame);
+}
+
+void rpm () {
+  if(total_time > 1){
+      total_time = 0;
+      hall_count = 0;
+   }
+  // preallocate values for tach
+  float start = micros();
+  bool on_state = false;
+  float rpmVal = 0;
+  
+  // counting number of times the hall sensor is tripped
+  // but without double counting during the same trip
+  while(true){
+    if (digitalRead(hall_pin)==1){
+      if (on_state==false){
+        on_state = true;
+        hall_count+=1;
+       // Serial.println("on");
+      }
+    }
+    else{
+      on_state = false;
+      break;
+    }
+  
+  }
+  // print information about Time and RPM
+  float end_time = micros();
+  total_time = total_time+((end_time-start)/1000000.0);
+  Serial.print("Time Passed: ");
+  Serial.print(total_time);
+  Serial.println("s");
+  float rpm_val = ((float)hall_count/total_time)*60.0;
+  Serial.println(digitalRead(hall_pin));
+  Serial.println(" RPM");
+  delay(1);        // delay in between reads for stability
 }
